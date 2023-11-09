6bd175f7db96e5f021a51e8ab218aa0b8ea8f907
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-09 10:58:14 +0000

    main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
-#include estudiante.h
+#include "estudiante.h"
 
+int main() {
+    Estudiante estudiante;
+
+    estudiante.nombre = "Binhao";
+    estudiante.edad = 18;
+    estudiante.grado = "Informatica";
+
+    estudiante.mostrar_info();
+
+    return 0;
+}
