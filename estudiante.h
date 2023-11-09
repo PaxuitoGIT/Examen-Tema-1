578ccd08b6cc3d21f0c8c0cf958b94a8670acc8c
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-09 10:38:51 +0000

    mostrar info estudiante

--- estudiante.h
+++ estudiante.h
@@ -3,4 +3,10 @@ class Estudiante {
         std::string nombre;
         int edad;
         std::string grado;
+
+        void mostrar_info() {
+            std::cout << "Nombre: " << nombre << std::endl;
+            std::cout << "Edad: " << edad << std::endl;
+            std::cout << "Grado: " << grado << std::endl;
+        }
 }
