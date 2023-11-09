1cd28f12b694227b603874be1c475c904382bea3
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-09 10:52:01 +0000

    excepcion estudiante

--- estudiante.h
+++ estudiante.h
@@ -5,6 +5,11 @@ class Estudiante {
         std::string grado;
 
         void mostrar_info() {
+            try {
+                if (nombre.empty() edad <= 0 grado.empty()) {
+                    throw std::string("Error: No se ha ingresado informacion del estudiante");
+                }
+            }
             std::cout << "Nombre: " << nombre << std::endl;
             std::cout << "Edad: " << edad << std::endl;
             std::cout << "Grado: " << grado << std::endl;
