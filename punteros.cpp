1c9ecdbc736dadc622dd83a07d0d0cab384d33fa
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-09 10:48:12 +0000

    Intercambio de valores del puntero

--- punteros.cpp
+++ punteros.cpp
@@ -5,3 +5,16 @@ void intercambiar(int *a, int *b) {
     *a = *b;
     *b = temp;
 }
+
+int main() {
+    int a = 3, b = 5;
+
+    std::cout <<"Valores a: " << a << " b: " << b << std::endl;
+
+    intercambiar(&a, &b);
+
+    std::cout  <<"Valores intercambiados a: " << a << " b: " << b << std::endl;
+
+    return 0;
+
+}
