b309aff233db7f1236768063f173ea568f589204
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-09 11:41:48 +0000

    Filtrar por grado

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@
 #include "promedioestudiante.h"
 #include "asistencia.h"
 
+std::vector<Estudiante> filtrar_por_grado(const std::vector<Estudiante>& estudiantes, const std::string& grado) {
+    std::vector<Estudiante> estudiantes_filtrados;
+
+    for (const auto& estudiante : estudiantes) {
+        if (estudiante.grado == grado) {
+            estudiantes_filtrados.push_back(estudiante);
+        }
+    }
+
+    return estudiantes_filtrados;
+}
+
 int main() {
     Estudiante estudiante;
 
