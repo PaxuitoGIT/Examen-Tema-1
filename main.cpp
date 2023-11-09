3ffdd2fecc2c772e08a73d20bebc7467e39ecce2
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-09 11:13:41 +0000

    mostrar materias en main

--- main.cpp
+++ main.cpp
@@ -8,7 +8,12 @@ int main() {
     estudiante.edad = 18;
     estudiante.grado = "Informatica";
 
+    estudiante.registrar_materia("Fisica");
+    estudiante.registrar_materia("Matematicas");
+    estudiante.registrar_materia("Programacion");
+
     estudiante.mostrar_info();
+    estudiante.mostrar_materias();
 
     return 0;
 }
